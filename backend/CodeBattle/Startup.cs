811b975397c2c6e558353f26e6721664235adc9b
EgorrrKuz <egor.player360@gmail.com>
2019-02-27 19:48:28 +0400

    Include SingnalR (WebSocket)

--- backend/CodeBattle/Startup.cs
+++ backend/CodeBattle/Startup.cs
@@ -5,19 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CodeBattle
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Инициализация сервиса SignalR
+            services.AddSignalR(hubOptions =>
+            {
+                // Возвращает клиенту детальное описание возникшей ошибки 
+                //(при ее возникновении)
+                hubOptions.EnableDetailedErrors = true;
+
+                // Если в течение этого периода сервер не отправит никаких сообшений,
+                // то автоматически отправляется ping-сообщение для поддержания подключения открытым
+                // (1 minute)
+                hubOptions.KeepAliveInterval = System.TimeSpan.FromMinutes(1); //
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,9 +35,21 @@ namespace CodeBattle
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseSignalR(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapHub<SignalR>("/signalr",
+                    options => {
+                        // Максимальный размер буфера в байтах, в который сервер помещает получаемые от клиента данные. (64)
+                        options.ApplicationMaxBufferSize = 64;
+                        // Максимальный размер буфера в байтах, в который сервер помещает данные для отправки клиенту. (64)
+                        options.TransportMaxBufferSize = 64;
+                        // Устанавливает периодичность опроса (1 minute)
+                        options.LongPolling.PollTimeout = System.TimeSpan.FromMinutes(1);
+                        // Настраивает транспорт WebSocket.
+                        options.Transports = HttpTransportType.LongPolling | HttpTransportType.WebSockets;
+                    });
             });
         }
     }
