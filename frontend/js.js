049b1a937c48c826d133964933a5ec5a10f265fe
Pavelruh <45849024+Pavelruh@users.noreply.github.com>
2019-03-14 16:07:22 +0400

    leaderboard
    сортировка по кол-ву очков

---
+++ frontend/js.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let elements = []
+  let container = document.querySelector('#container')
+  // Add each row to the array
+  container.querySelectorAll('.row').forEach(el => elements.push(el))
+  // Clear the container
+  container.innerHTML = ''
+  // Sort the array from highest to lowest
+  elements.sort((a, b) => b.querySelector('.score').textContent - a.querySelector('.score').textContent)
+  // Put the elements back into the container
+  elements.forEach(e => container.appendChild(e))
+})
+
+let Score = [];
+Score = document.querySelectorAll('score').textContent;
+console.log(Score);
