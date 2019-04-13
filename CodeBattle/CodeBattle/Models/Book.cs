7da059849c8d181d731171853ae0ce9fa9539e6b
JackBlackmlg <jackblackmlg1@gmail.com>
2019-04-13 14:04:39 +0400

    теперь доллжно работать

--- CodeBattle/CodeBattle/Models/Book.cs
+++
@@ -1,8 +0,0 @@
-﻿using Newtonsoft.Json;
-public class Book
-{
-    [JsonProperty("name")]
-    public string Name { get; set; }
-    [JsonProperty("author")]
-    public string Author { get; set; }
-}
---
+++ CodeBattle/Controllers/MapController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using CodeBattle.Models;
+using CodeBattle.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace CodeBattle.Controllers
+{
+    [Route("api-v1/[controller]")]
+    public class MapController : Controller
+    {
+        private readonly MapService _MapService;
+
+        public MapController(MapService mapService)
+        {
+            _MapService = mapService;
+            
+        }
+        // TODO словарь некорректно(None) преобразуется в BsonElement когда отсылается в коллекцию
+        [HttpPost]
+        public ActionResult Post(object books)
+        {
+            /* var map_create =_MapService.Create(map);
+             if(map_create == null)
+             {
+                 return NoContent();
+             }
+             else
+             {
+                 return map;
+             }*/
+            return Json(books);
+        }
+        
+        [HttpGet("{index:max(50)}")]
+        public ActionResult<Map> Get(int index)
+        {
+            var map_get = _MapService.Get(index);
+            if (map_get == null || index == 0)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return map_get;
+            }
+        }
+    }
+}
