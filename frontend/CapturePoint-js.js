b6d1d2697a051a5bd844eb8e434c85444585c283
student <student@obr18.ru>
2019-04-29 18:12:48 +0400

    server connection is added

--- frontend/CapturePoint-js.js
+++ frontend/CapturePoint-js.js
@@ -50,3 +50,57 @@ function Show ()
   if ( elem ) 
     elem.style.display = "block";
 }
+/*
+Функция посылки запроса к файлу на сервере
+r_method  - тип запроса: GET или POST
+r_path    - путь к файлу
+r_args    - аргументы вида a=1&b=2&c=3...
+r_handler - функция-обработчик ответа от сервера
+*/
+function SendRequest(r_method, r_path, r_args, r_handler)
+{
+    //Создаём запрос
+    var Request = CreateRequest();
+    
+    //Проверяем существование запроса еще раз
+    if (!Request)
+    {
+        return;
+    }
+    
+    //Назначаем пользовательский обработчик
+    Request.onreadystatechange = function()
+    {
+        //Если обмен данными завершен
+        if (Request.readyState == 4)
+        {
+            //Передаем управление обработчику пользователя
+            r_handler(Request);
+        }
+    }
+    
+    //Проверяем, если требуется сделать GET-запрос
+    if (r_method.toLowerCase() == "get" && r_args.length > 0)
+    r_path += "?" + r_args;
+    
+    //Инициализируем соединение
+    Request.open(r_method, r_path, true);
+    
+    if (r_method.toLowerCase() == "post")
+    {
+        //Если это POST-запрос
+        
+        //Устанавливаем заголовок
+        Request.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=utf-8");
+        //Посылаем запрос
+        Request.send(r_args);
+    }
+    else
+    {
+        //Если это GET-запрос
+        
+        //Посылаем нуль-запрос
+        Request.send(null);
+    }
+} 
+
